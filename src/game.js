585836711e060ebbf4d294387381318b79e6fbe4
Student <example@example.com>
2020-02-12 11:45:13 -0800

    on button click display a cat thats either fake or real

--- src/game.js
+++ src/game.js
@@ -1,13 +1,19 @@
 
 // Create base values for user
-export class Tamagotchi {
-  constructor(realFace, fakeFace) {
-   this.realFace = realFace;
-   this.fakeFace = fakeFace;
-   this.faceArray=[];
+export class Game {
+  constructor() {
+   this.realCat = '';
+   this.fakeCat = '';
+   this.catArray=[];
    this.score = 0;
 
   }
-
-  
+  addFake(cat){
+    this.fakeCat = cat
+    this.catArray.push(cat)
+  }
+  addReal(cat){
+    this.realCat = cat
+    this.catArray.push(cat)
+  }
 }
--- src/main.js
+++ src/main.js
@@ -1,9 +1,38 @@
 
-//import './styles.css';
+import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { FakeCat } from './fakeCat-service';
+import { Game } from './game';
+import { RealCat } from './trueCat-service';
+
 
 
 $(document).ready(function () {
-})
+    let game = new Game
+      $('#start').click(()=>{
+        (async () => {
+            game.catArray = [];
+            let fakeCat = new FakeCat();
+            let realCat = new RealCat()
+            await fakeCat.getFakeCat()
+            await realCat.getRealCat()
+            game.addFake(fakeCat.cat)
+            game.addReal(realCat.cat)
+            let rand = Math.round(Math.random());
+            console.log(rand);
+            console.log(game.catArray);
+            
+            $('#fake').attr('src', game.catArray[rand])
+           })()
+
+          
+        //   $('#real').attr('src', game.realCat)
+
+
+
+      })
+
+
+    })
